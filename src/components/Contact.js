e1a3e7a7861c734918bbb66bbee341d57dde91d8
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2023-02-07 15:12:08 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,31 @@
 import React from "react";
 
 const Contact = () => {
-  return (
-{/* <section id="contactMe" className="container">
-      <div className="floatLeft">
-        <form className="form">
-        <div className="pageTitle title">Let's Connect! </div>
-        <div className="secondaryTitle title">
-          Let's work together.
-        </div> 
-        <input type="text" className="name formEntry" placeholder="Name" />
-        <input type="text" className="email formEntry" placeholder="Email" />
-        <textarea
-          className="message formEntry"
-          placeholder="Message"
-          ></textarea>
-        <input type="checkbox" className="termsConditions" value="Term" />
-        <label className="terms">
-          Captch Captcha.
-        </label>
-        <button className="submit formEntry">Submit</button>
-          </form>
-      </div>
-    </section> */}
-  );
+  return <section></section>;
 };
 
 export default Contact;
 
 {
-  /* // <section id="contactMe" className="container">
-      //   <div>
-      //     <h2>Let's work together!</h2>
-      //     <a className="link" href="mailto:landonaugust@protonmail.com">
-      //       Email
-      //     </a>
-      //   </div>
-      // </section> */
+  /* <section id="contactMe" className="container">
+        <div className="floatLeft">
+          <form className="form">
+          <div className="pageTitle title">Let's Connect! </div>
+          <div className="secondaryTitle title">
+            Let's work together.
+          </div> 
+          <input type="text" className="name formEntry" placeholder="Name" />
+          <input type="text" className="email formEntry" placeholder="Email" />
+          <textarea
+            className="message formEntry"
+            placeholder="Message"
+            ></textarea>
+          <input type="checkbox" className="termsConditions" value="Term" />
+          <label className="terms">
+            Captch Captcha.
+          </label>
+          <button className="submit formEntry">Submit</button>
+            </form>
+        </div>
+      </section> */
 }
